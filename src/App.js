487d36728200d184b2f235754fe17dbffabb635b
Hamza405 <hamzash863@gmail.com>
2022-10-18 05:14:16 -0400

    file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import {
+  Button,
+  Container,
+  IconButton,
+  Typography,
+  Stack,
+  Fab,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Divider,
+  List,
+  ListItem,
+  Tooltip,
+  Card,
+} from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
+
 function App() {
-  return <div className="">social App</div>;
+  return <Container sx={{ mt: 11 }}></Container>;
 }
 
 export default App;
